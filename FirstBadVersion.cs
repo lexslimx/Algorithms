005dcaa17e992f7f5adeaa7c3db4093fc62cb404
alex <alex.km@live.com>
2021-11-04 23:50:13 +0300

    Added search insert pos with bbionary search

--- FirstBadVersion.cs
+++ FirstBadVersion.cs
@@ -11,6 +11,24 @@ namespace Algorithms
             return 0;
         }
 
+        public int FindirstBadVersioWithBinarySearch(int n)
+        {
+
+            int left = 1;
+            int right = n;            
+            while(left < right){
+                int mid = left + (right-left)/2;
+                if(!IsBadVersion(mid)){
+                    left = mid+1; //SEAERCH RIGHT
+                }
+                else{
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+
         bool IsBadVersion(int version){
 
             return false;
